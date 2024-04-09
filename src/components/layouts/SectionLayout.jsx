4830b32428ce812bf4df90cecf9f0072ab3d61cf
Alejandro Centurion <alejandrocenturion@outlook.com>
2024-04-09 03:27:29 -0300

    fix css

--- src/components/layouts/SectionLayout.jsx
+++ src/components/layouts/SectionLayout.jsx
@@ -1,6 +1,6 @@
 function SectionLayout({children, id, className=''}) {
     return (
-        <section id={id} className={"flex flex-col md:flex-row w-full px-2 py-4 md:py-8 md:px-8 lg:px-24 lg:py-16 " + className}>
+        <section id={id} className={"flex flex-col md:flex-row w-full px-4 py-8 md:py-8 md:px-8 lg:px-24 lg:py-16 " + className}>
             {children}
         </section>
     );
--- src/components/organism/ContactUs.jsx
+++ src/components/organism/ContactUs.jsx
@@ -3,9 +3,9 @@ import SectionLayout from "../layouts/SectionLayout.jsx";
 
 export default function ContactUs() {
     return (
-        <SectionLayout id={"contact"} className={"md:flex-col"}>
-            <div className={"lg:flex-col mb-8"}>
-                <h2 className={"text-3xl lg:text-5xl font-bold text-secondary-950 pb-4 mb-4 text-left"}>Contactános</h2>
+        <SectionLayout id={"contact"} className={"md:flex-col gap-4"}>
+            <div className={"lg:flex-col"}>
+                <h2 className={"text-3xl lg:text-5xl font-bold text-secondary-950 pb-4 text-left"}>Contactános</h2>
                 <p className={"text-xl lg:text-2xl text-secondary-950 pb-4 text-left"}>Comunicate con nosotros para consultas de operaciones o ventas.</p>
             </div>
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-8">
