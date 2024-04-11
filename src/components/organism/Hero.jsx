e1dd2b43fab056896f07ae6552d585f2ef61b779
Alejandro Centurion <alejandrocenturion@outlook.com>
2024-04-11 17:14:39 -0300

    changed hero

--- src/components/organism/Hero.jsx
+++ src/components/organism/Hero.jsx
@@ -19,12 +19,12 @@ function Hero() {
             'md:px-24 ' +
             'pt-16 lg:pt-0 ' +
             'bg-cover ' +
-            'md:bg-[length:165%] lg:bg-[length:100%]'
+            'md:bg-[length:165%] lg:bg-[length:125%] bg-[-100px_0] lg:bg-[0]'
         }
                  style={{backgroundImage: `url(${portada})`}}
         >
             <Logo className={"w-64 lg:w-[350px]"}/>
-            <h1 className={"text-5xl md:text-7xl lg:text-8xl lg:font-black text-secondary-50 font-bold text-center md:text-left"}>Tu
+            <h1 className={"text-5xl md:text-7xl lg:text-8xl lg:font-black text-secondary-100 font-bold text-center md:text-left"}>Tu
                 depósito <br/> de confianza.</h1>
             <ButtonContact className={"md:text-2xl lg:text-3xl lg:font-bold md:py-2 md:px-6 lg:py-4 lg:px-6"}/>
         </section>
