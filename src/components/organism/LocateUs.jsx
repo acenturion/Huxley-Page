bdd9c17d14e81345de1ccca8c26dd9a3b9070247
Alejandro Centurion <alejandrocenturion@outlook.com>
2024-04-11 17:03:23 -0300

    Update LocateUs.jsx

--- src/components/organism/LocateUs.jsx
+++ src/components/organism/LocateUs.jsx
@@ -8,17 +8,17 @@ function LocateUs() {
                 href={"https://maps.app.goo.gl/biiyFGnTCBgZpfWm7"}
                 target="_blank"
                 rel="noopener"
-                className={"w-full h-[200px] md:h-full max-w-[600px] border border-primary-600 bg-primary-200 overflow-clip shadow"}>
+                className={"w-full h-[320px] md:h-full max-w-[600px] border border-primary-600 bg-primary-200 overflow-clip shadow"}>
                 <img className={"w-full h-full max object-cover transition hover:scale-110"}
                      alt={"Mapa donde se encuentra Huxley"} src={map}/>
             </a>
             <div className={"text-left"}>
                 <div className={"border-b border-primary-600 pb-4 mb-8"}>
-                    <h3 className={"text-2xl lg:text-4xl text-secondary-950 pb-4"}>Dirección</h3>
+                    <h3 className={"text-2xl lg:text-4xl text-secondary-950 pb-4 font-semibold"}>Dirección</h3>
                     <p className={"text-xl lg:text-2xl text-secondary-950 pb-4"}>Av. Suarez 2350, Barracas, CABA.</p>
                 </div>
                 <div>
-                    <h3 className={"text-2xl lg:text-4xl text-secondary-950 pb-4"}>Horarios</h3>
+                    <h3 className={"text-2xl lg:text-4xl text-secondary-950 pb-4 font-semibold"}>Horarios</h3>
                     <p className={"text-xl lg:text-2xl text-secondary-950 pb-2"}>
                         Lunes a Viernes de <strong>8 a 17:30 Hs</strong>.
                     </p>
