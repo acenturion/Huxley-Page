4c5e13bd8c8fc2755619f61587bb77dd0b6f8ecd
Alejandro Centurion <alejandrocenturion@outlook.com>
2024-05-10 01:01:40 -0300

    Update Companies.jsx

--- src/components/organism/Companies.jsx
+++ src/components/organism/Companies.jsx
@@ -9,9 +9,9 @@ function Companies() {
     const brands = [
         {id: 1, name: "Hapag Lloyd", img: hapagLogo},
         {id: 2, name: "Hamburg Sud", img: hsLogo},
-        {id: 4, name: "Mediterranean shipping company", img: mediterraneanLogo, customClass: "h-full"},
         {id: 3, name: "Maesrk", img: maesrkLogo, customClass: "h-full p-4"},
         {id: 5, name: "Triton", img: tritonLogo},
+        {id: 4, name: "Mediterranean shipping company", img: mediterraneanLogo, customClass: "h-full p-4 md:p-0"},
     ]
     return (
         <SectionLayout id={"companies"} className={"bg-primary-600 gap-8 md:flex-col lg:flex-row"}>
@@ -24,7 +24,7 @@ function Companies() {
             <div className={"grid grid-cols-2 gap-4 lg:grid-cols-5 w-full"}>
                 {brands.map(brand => {
                     return (
-                        <div key={brand.id} className={"w-full h-[75px] flex justify-center items-center object-fill"}>
+                        <div key={brand.id} className={"w-full h-[100px] flex justify-center items-center object-fill"}>
                             <img
                                  className={"w-full transition hover:scale-110 focus:scale-110 lg:grayscale hover:lg:grayscale-0 " + brand?.customClass }
                                  alt={brand.name + " logo"} src={brand.img}/>
