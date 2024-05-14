4a49f78d58894dd8355278c2283a0bb4d2dc4197
Alejandro Centurion <alejandrocenturion@outlook.com>
2024-05-14 07:51:36 -0300

    fixed logo colors

--- src/components/organism/Companies.jsx
+++ src/components/organism/Companies.jsx
@@ -9,9 +9,9 @@ function Companies() {
     const brands = [
         {id: 1, name: "Hapag Lloyd", img: hapagLogo},
         {id: 2, name: "Hamburg Sud", img: hsLogo},
-        {id: 3, name: "Maesrk", img: maesrkLogo, customClass: "h-full p-4"},
+        {id: 3, name: "Maesrk", img: maesrkLogo},
         {id: 5, name: "Triton", img: tritonLogo},
-        {id: 4, name: "Mediterranean shipping company", img: mediterraneanLogo, customClass: "p-4 md:p-0"},
+        {id: 4, name: "Mediterranean shipping company", img: mediterraneanLogo, customClass: " md:p-0"},
     ]
     return (
         <SectionLayout id={"companies"} className={"bg-primary-600 gap-8 md:flex-col lg:flex-row"}>
@@ -21,13 +21,14 @@ function Companies() {
                     className={"text-2xl md:text-3xl font-semibold text-nowrap"}>Los siguientes clientes</span>
                 </p>
             </div>
-            <div className={"grid grid-cols-2 gap-4 lg:grid-cols-5 w-full"}>
+            <div className={"flex flex-wrap justify-center gap-4 lg:grid lg:grid-cols-5 w-full"}>
                 {brands.map(brand => {
                     return (
-                        <div key={brand.id} className={"w-full h-[100px] flex justify-center items-center object-fill"}>
+                        <div key={brand.id}
+                             className={"w-[45%] lg:w-full h-[100px] flex justify-center items-center object-contain"}>
                             <img
-                                 className={"w-full transition hover:scale-110 focus:scale-110 " + brand?.customClass }
-                                 alt={brand.name + " logo"} src={brand.img}/>
+                                className={"w-full transition hover:scale-110 focus:scale-110 " + brand?.customClass}
+                                alt={brand.name + " logo"} src={brand.img}/>
                         </div>
                     );
                 })}
