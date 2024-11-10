35aae1f9259856a072dc9e0295e2a54b43b06974
Alejandro Centurion <alejandrocenturion@outlook.com>
2024-11-09 22:03:50 -0300

    agregado fotos

--- src/components/organism/Explore.jsx
+++ src/components/organism/Explore.jsx
@@ -10,18 +10,36 @@ import imgService9 from "../../assets/img/9.webp";
 import imgService11 from "../../assets/img/11.webp";
 import imgService15 from "../../assets/img/15.webp";
 import imgService19 from "../../assets/img/19.webp";
+import imgNew2 from "../../assets/img/n2.webp";
+import imgNew3 from "../../assets/img/n3.webp";
+import imgNew4 from "../../assets/img/n4.webp";
+import imgNew5 from "../../assets/img/n5.webp";
+import imgNew6 from "../../assets/img/n6.webp";
+import imgNew7 from "../../assets/img/n7.webp";
+import imgNew8 from "../../assets/img/n8.webp";
+import imgNew9 from "../../assets/img/n9.webp";
+
+
 import ControlSlider from "../molecules/ControlSlider.jsx";
 
 const images = [
-    { id: 8, url: imgService8 },
-    { id: 2, url: imgService2 },
-    { id: 4, url: imgService4 },
-    { id: 5, url: imgService5 },
-    { id: 6, url: imgService6 },
-    { id: 9, url: imgService9 },
-    { id: 11, url: imgService11 },
-    { id: 15, url: imgService15 },
-    { id: 19, url: imgService19 },
+    {id: 1, url: imgService8},
+    {id: 2, url: imgNew2},
+    {id: 3, url: imgService2},
+    {id: 4, url: imgNew3},
+    {id: 5, url: imgService4},
+    {id: 6, url: imgNew4},
+    {id: 7, url: imgService5},
+    {id: 8, url: imgNew5},
+    {id: 9, url: imgService6},
+    {id: 10, url: imgNew6},
+    {id: 11, url: imgService9},
+    {id: 12, url: imgNew7},
+    {id: 13, url: imgService11},
+    {id: 14, url: imgNew8},
+    {id: 15, url: imgService15},
+    {id: 16, url: imgNew9},
+    {id: 17, url: imgService19},
 ];
 
 function Explore() {
@@ -37,10 +55,12 @@ function Explore() {
     }
     return (
         <section id={"explore"} className={"py-8"}>
-            <div className={"w-full flex flex-row justify-between px-4 md:px-8 lg:px-24 mb-8"} >
+            <div className={"w-full flex flex-row justify-between px-4 md:px-8 lg:px-24 mb-8"}>
                 <div>
-                    <h2 className={"text-3xl md:text-5xl font-bold text-secondary-950 pb-4 mb-4 text-left"}>Explora Huxley</h2>
-                    <p className={"text-xl md:text-2xl text-secondary-950 pb-4 text-left"}>Descubrí las instalaciones y servicios
+                    <h2 className={"text-3xl md:text-5xl font-bold text-secondary-950 pb-4 mb-4 text-left"}>Explora
+                        Huxley</h2>
+                    <p className={"text-xl md:text-2xl text-secondary-950 pb-4 text-left"}>Descubrí las instalaciones y
+                        servicios
                         disponibles.</p>
                 </div>
                 <ControlSlider
@@ -64,7 +84,8 @@ function Explore() {
             >
                 {images.map(({url, id}) => (
                     <SplideSlide key={id}>
-                        <img className={"h-full w-full aspect-[16/9] object-cover object-center"} width={"500"} height={"400"} src={url} alt={"Image "+ id}/>
+                        <img className={"h-full w-full aspect-[16/9] object-cover object-center"} width={"500"}
+                             height={"400"} src={url} alt={"Image " + id}/>
                     </SplideSlide>
                 ))}
             </Splide>
